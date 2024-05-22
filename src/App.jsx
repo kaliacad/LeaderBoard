bac14837a16fc403b99fbb9f9be948db06e828e1
firmin nagnduli <finganduli@gmail.com>
2024-05-22 19:41:14 +0200

    Display contributions for one user

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,113 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [usernames, setUsernames] = useState('');
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
+  const [usernames, setUsernames] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
   const [loading, setLoading] = useState(false);
-  const [resultWikipedia, setResultWikipedia] = useState('');
-  const [resultCommons, setResultCommons] = useState('');
+  const [resultWikipedia, setResultWikipedia] = useState(
+    "the result will be displayed here"
+  );
+  const [resultCount, setResultCount] = useState(-1);
+  const [resultCommons, setResultCommons] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
 
     // Simulation de résultats après une requête
-    setTimeout(() => {
+    setTimeout(async () => {
       setLoading(false);
-      setResultWikipedia('Résultats Wikipedia...');
-      setResultCommons('Résultats Commons...');
+      let result = await fetch(
+        `https://fr.wikipedia.org/w/api.php?action=query&list=usercontribs&ucuser=${usernames}&uclimit=500&ucprop=title|timestamp&format=json&origin=*`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setResultWikipedia(data.query.usercontribs);
+          setResultCount(resultWikipedia.length);
+        });
+
+      setResultCommons("Résultats Commons...");
     }, 2000);
   };
 
+  function dateformat(date) {
+    let firstD = date.split("T")[0];
+    let secondD = date.split("T")[1].replace("Z", "");
+    date = firstD.split("T")[0].replaceAll("-", " ").split(" ");
+
+    return `${date[2]}-${date[1]}-${date[0]} ${secondD}`;
+  }
   return (
     <div className="container">
       <h1>Comparer les contributions Wikipedia </h1>
-      <form id="userForm" onSubmit={handleSubmit}>
-        <label htmlFor="usernames">Noms d'utilisateur (séparés par des virgules):</label>
-        <input 
-          type="text" 
-          id="usernames" 
-          name="usernames" 
-          value={usernames} 
-          onChange={(e) => setUsernames(e.target.value)} 
-          required 
-        />
-        
-        <label htmlFor="startDate">Date de début:</label>
-        <input 
-          type="date" 
-          id="startDate" 
-          name="startDate" 
-          value={startDate} 
-          onChange={(e) => setStartDate(e.target.value)} 
-          required 
-        />
-        
-        <label htmlFor="endDate">Date de fin:</label>
-        <input 
-          type="date" 
-          id="endDate" 
-          name="endDate" 
-          value={endDate} 
-          onChange={(e) => setEndDate(e.target.value)} 
-          required 
-        />
-        
-        <button type="submit">Comparer</button>
-      </form>
-      {loading && <div id="loader" className="loader"></div>}
-      <div id="resultWikipedia">{resultWikipedia}</div>
+      <div className="form-container">
+        <form id="userForm" onSubmit={handleSubmit}>
+          <label htmlFor="usernames">
+            Noms d'utilisateur (séparés par des virgules):
+          </label>
+          <input
+            type="text"
+            id="usernames"
+            name="usernames"
+            value={usernames}
+            onChange={(e) => setUsernames(e.target.value)}
+            required
+          />
+
+          <label htmlFor="startDate">Date de début:</label>
+          <input
+            type="date"
+            id="startDate"
+            name="startDate"
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+            required
+          />
+
+          <label htmlFor="endDate">Date de fin:</label>
+          <input
+            type="date"
+            id="endDate"
+            name="endDate"
+            value={endDate}
+            onChange={(e) => setEndDate(e.target.value)}
+            required
+          />
+
+          <button type="submit">Comparer</button>
+        </form>
+        <div>
+          {loading && <div id="loader" className="loader"></div>}
+          <div id="resultWikipedia">
+            {resultCount < 0 ? (
+              "the result will be displayed here"
+            ) : resultWikipedia.length == 0 ? (
+              "there is not result for that user"
+            ) : (
+              <>
+                <h4 className="resultTitle">
+                  The result for the user {usernames} are{" "}
+                  {resultWikipedia.length}
+                </h4>
+                <div className="result">
+                  <h5>user</h5>
+                  <h5>Title</h5>
+                  <h5>Date </h5>
+                </div>
+                {resultWikipedia?.map((el, index) => (
+                  <div key={index} className="result">
+                    <h6> {el.user}</h6>
+                    <h6>{el.title}</h6>
+                    <h6>{dateformat(el.timestamp)}</h6>
+                  </div>
+                ))}
+              </>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
